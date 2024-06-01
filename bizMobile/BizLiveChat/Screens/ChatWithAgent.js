4487fb97b5806d876010faf8f38b2b02fe53152a
Khalifa Dalhat <khalifadalhat@gmail.com>
2024-06-01 20:55:03 +0100

    added local host as the ip address

--- bizMobile/BizLiveChat/Screens/ChatWithAgent.js
+++ bizMobile/BizLiveChat/Screens/ChatWithAgent.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, FlatList, StyleSheet } from 'react-native';
 import io from 'socket.io-client';
 
-const socket = io('http://172.20.10.3:5000', {
+const socket = io('http://localhost:5000', {
   withCredentials: true,
 });
 
