1c3a866f12c84f23be25b88705d8a56d9e0547dc
khalifa <khalifadalhat@gmail.com>
2025-07-03 22:26:57 +0100

    feat: replace DashboardDetails with LandingPage in App component for improved user experience

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,14 +6,14 @@ import SupervisorDashboard from './pages/supervisor/SupervisorDashboard';
 import Login from './pages/Login';
 import SignupForm from './pages/SignUp';
 import CustomerDashboard from './pages/customer/CustomerDashboard';
-import DashboardDetails from './pages/DashboardDetails';
 import CustomerRegistration from './pages/customer/customerRegistration';
 import CustomerLogin from './pages/customer/customerLogin';
+import LandingPage from './components/LandingPage';
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<DashboardDetails />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignupForm />} />
       <Route path="/signup/customer" element={<CustomerRegistration />} />
