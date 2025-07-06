30a89608318a11337bcd9a2cadc28836ee5c8e2f
khalifa <khalifadalhat@gmail.com>
2025-07-06 16:10:47 +0100

    feat: add axios API utility with request interceptor for token management

---
+++ client/src/utils/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const API_URL = import.meta.env.VITE_API_URL as string;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
