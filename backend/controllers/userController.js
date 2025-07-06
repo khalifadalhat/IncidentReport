9e18957fcb795464fa8a53bac2eea3f6df63a3b2
khalifa <khalifadalhat@gmail.com>
2025-07-06 15:18:20 +0100

    refactor: streamline user creation process by removing password hashing and enhancing user response structure

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,5 @@
-const User = require('../models/user');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 exports.createUser = async (req, res) => {
   try {
@@ -8,34 +7,36 @@ exports.createUser = async (req, res) => {
 
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ error: 'Username already exists' });
+      return res.status(400).json({ error: "Username already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({ email, password: hashedPassword, role });
+    const newUser = new User({ email, password, role });
 
     await newUser.save();
 
-    const token = jwt.sign({ userId: newUser._id }, 'BIZZAPP');
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
     newUser.token = token;
     await newUser.save();
 
-    res.cookie('token', token, { httpOnly: true }); 
-    res.status(201).json({ user: newUser, token }); 
+    res.cookie("token", token, { httpOnly: true });
+    res.status(201).json({
+      user: {
+        id: newUser._id,
+        email: newUser.email,
+        role: newUser.role,
+      },
+      token,
+    });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 };
 
-
-exports.getUsers = [
-  async (req, res) => {
-    try {
-      const users = await User.find();
-      res.status(200).json({ users });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-];
+};
