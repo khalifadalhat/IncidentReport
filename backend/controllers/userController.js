10c0f65284717d221dc988f937c4b6d0b6492dbb
khalifa <khalifadalhat@gmail.com>
2025-07-06 12:41:50 +0100

    fix: update user creation logic to use email instead of username for uniqueness check

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,16 +4,16 @@ const jwt = require('jsonwebtoken');
 
 exports.createUser = async (req, res) => {
   try {
-    const { username, password, role } = req.body;
+    const { email, password, role } = req.body;
 
-    const existingUser = await User.findOne({ username });
+    const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ error: 'Username already exists' });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newUser = new User({ username, password: hashedPassword, role });
+    const newUser = new User({ email, password: hashedPassword, role });
 
     await newUser.save();
 
