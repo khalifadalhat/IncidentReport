86774cc8317a0b2e02151e720cd34fcf1cb475df
khalifa <khalifadalhat@gmail.com>
2025-11-29 16:10:55 +0100

    feat: add useLogin hook for user authentication and navigation
    This commit introduces the `useLogin` hook, which facilitates user authentication through a mutation. Key features include:
    
    - Integration with the authentication store to manage login functionality.
    - Conditional navigation based on user roles (customer, agent, admin) upon successful login.
    - Streamlined user experience by directing users to their respective dashboards.
    
    These additions enhance the application's authentication process and improve user flow after login.

---
+++ client/src/hook/useLogin.ts
@@ -0,0 +1,20 @@
+import { useAuthStore } from "@/store/useAuthStore";
+import { useMutation } from "@tanstack/react-query";
+
+import { useNavigate } from "react-router-dom";
+
+export const useLogin = () => {
+  const { login } = useAuthStore();
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: ({ email, password }: { email: string; password: string }) =>
+      login(email, password),
+    onSuccess: (_, variables) => {
+      const role = useAuthStore.getState().user?.role;
+      if (role === "customer") navigate("/customer");
+      else if (role === "agent") navigate("/agent");
+      else if (role === "admin") navigate("/admin");
+    },
+  });
+};
