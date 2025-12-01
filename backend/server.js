3fa70170dfa17fd5924bbcd50f6c8cdd4fa81db6
khalifa <khalifadalhat@gmail.com>
2025-12-01 22:40:40 +0100

    fix: correct allowedOrigins format in server configuration
    This commit updates the allowedOrigins configuration in the server.js file by removing the trailing slash from the localhost URL. This change ensures proper handling of CORS requests, improving the server's compatibility with frontend applications.

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 
 const allowedOrigins = process.env.ALLOWED_ORIGINS
   ? process.env.ALLOWED_ORIGINS.split(",")
-  : ["http://localhost:5173/"];
+  : ["http://localhost:5173"];
 
 app.use(
   cors({
