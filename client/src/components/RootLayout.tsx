e6571f86d737b81c116e660c00647877de7cdec1
khalifa <khalifadalhat@gmail.com>
2025-12-22 20:49:15 +0100

    refactor: remove NotificationDropdown from RootLayout to simplify component structure

--- client/src/components/RootLayout.tsx
+++ client/src/components/RootLayout.tsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router-dom";
 import NotificationManager from "./NotificationManager";
-import NotificationDropdown from "./NotificationContent";
 
 const RootLayout = () => {
   return (
     <>
       <NotificationManager />
-      <NotificationDropdown />
       <Outlet />
     </>
   );
